183be3a6ec85f33cf460c0bd4e41b0f2c6b3569a
M-imo <97694145+M-imo@users.noreply.github.com>
2023-01-11 19:47:12 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:drawer_example/contacts.dart';
+import 'package:drawer_example/dashboard.dart';
+import 'package:drawer_example/events.dart';
+import 'package:drawer_example/notes.dart';
+import 'package:drawer_example/notifications.dart';
+import 'package:drawer_example/privacy_policy.dart';
+import 'package:drawer_example/send_feedback.dart';
+import 'package:drawer_example/settings.dart';
+import 'package:flutter/material.dart';
+
+import 'my_header_drawer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentPage = DrawerSections.dashboard;
+
+  @override
+  Widget build(BuildContext context) {
+    var container;
+    if (currentPage == DrawerSections.dashboard) {
+      container = DashboardPage();
+    } else if (currentPage == DrawerSections.contacts) {
+      container = ContactsPage();
+    } else if (currentPage == DrawerSections.events) {
+      container = EventsPage();
+    } else if (currentPage == DrawerSections.notes) {
+      container = NotesPage();
+    } else if (currentPage == DrawerSections.settings) {
+      container = SettingsPage();
+    } else if (currentPage == DrawerSections.notifications) {
+      container = NotificationsPage();
+    } else if (currentPage == DrawerSections.privacyPolicy) {
+      container = PrivacyPolicyPage();
+    } else if (currentPage == DrawerSections.sendFeedback) {
+      container = SendFeedbackPage();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink[700],
+        title: Text("Fancy Cat"),
+      ),
+      body: container,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                MyHeaderDrawer(),
+                MyDrawerList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyDrawerList() {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        //Show the list of menu drawer!!
+        children: [
+          menuItem(1, "Dashboard", Icons.dashboard_outlined,
+              currentPage == DrawerSections.dashboard ? true : false),
+          menuItem(2, "Contacts", Icons.people_alt_outlined,
+              currentPage == DrawerSections.contacts ? true : false),
+          menuItem(3, "Events", Icons.event,
+              currentPage == DrawerSections.events ? true : false),
+          menuItem(4, "Notes", Icons.notes,
+              currentPage == DrawerSections.notes ? true : false),
+          menuItem(5, "Settings", Icons.settings_outlined,
+              currentPage == DrawerSections.settings ? true : false),
+          menuItem(6, "Notifications", Icons.notifications_outlined,
+              currentPage == DrawerSections.notifications ? true : false),
+          menuItem(7, "Privacy policy", Icons.privacy_tip_outlined,
+              currentPage == DrawerSections.privacyPolicy ? true : false),
+          menuItem(8, "Send feedback", Icons.feed_outlined,
+              currentPage == DrawerSections.sendFeedback ? true : false),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSections.dashboard;
+            } else if (id == 2) {
+              currentPage = DrawerSections.contacts;
+            } else if (id == 3) {
+              currentPage = DrawerSections.events;
+            } else if (id == 4) {
+              currentPage = DrawerSections.notes;
+            } else if (id == 5) {
+              currentPage = DrawerSections.settings;
+            } else if (id == 6) {
+              currentPage = DrawerSections.notifications;
+            } else if (id == 7) {
+              currentPage = DrawerSections.privacyPolicy;
+            } else if (id == 8) {
+              currentPage = DrawerSections.sendFeedback;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum DrawerSections {
+  dashboard,
+  contacts,
+  events,
+  notes,
+  settings,
+  notifications,
+  privacyPolicy,
+  sendFeedback,
+}
---
+++ lib/send_feedback.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SendFeedbackPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Send feedback Page"),
+      ),
+    );
+  }
+}

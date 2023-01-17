f2035e69e78b9c9019f2214157ae442cae350158
samakab99 <samakab4@gmail.com>
2023-01-17 11:31:42 +0100

    La til denne teksten i specific_requirements.dart  :
    The team is often expected to come up with a near- perfect requirements document and design before they start building code, because it takes a lot of time and effort to go back and fix the requirements and design when the team finds problems with them.

--- lib/specific_requirements.dart
+++ lib/specific_requirements.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class SpecificRequirementsPage extends StatelessWidget {
@@ -15,7 +17,22 @@ class SpecificRequirementsPage extends StatelessWidget {
             "Sources: \n"
             "ChatGTP. (2020). ChatGTP. Retrieved 15.01.2023 from https://www.chatgtp.com/."
             ""),
+
+
+
+        child: Text("\n"
+            "\n"
+            "Specific Requirements: The Team  \n" "\n"
+            "is often expected to come up with a near- perfect requirements document and design before they start building code."
+            "because it takes a lot of time and effort to go back and fix the requirements and design when the team finds problems with them.
+        "
+            "A requirements specification typically includes a detailed description of the system's functional and non-functional requirements, and may also include a set of use cases that describe how the system should respond to particular inputs.\n"
+            "\n"
+            "Sources: \n"
+            "Head First: Agile. (2017), Page 67." "\n"
+            "Simplified by: Chatgpt"),
       ),
     );
   }
 }
+
